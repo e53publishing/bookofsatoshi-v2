ad016e3f1743c5e2b49ef58dcad73325241a896a
Samuel Champagne <17770653+astraloverflow@users.noreply.github.com>
2025-10-30 11:28:13 -0500

    Refactor .stylelintrc.cjs for consistent quotation style and add import-notation rule

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
-  plugins: ['stylelint-order'],
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-order"],
   rules: {
     // Enforce grouping by type
-    'order/order': ['dollar-variables', 'custom-properties', 'declarations'],
+    "order/order": ["dollar-variables", "custom-properties", "declarations"],
     // Set to null if using something like tailwind's postcss module
-    'at-rule-no-unknown': null,
+    "at-rule-no-unknown": null,
+    "import-notation": null,
   },
-  ignoreFiles: ['node_modules/**', 'dist/**'],
+  ignoreFiles: ["node_modules/**", "dist/**"],
 };
