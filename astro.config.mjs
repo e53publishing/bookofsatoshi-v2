1cfa500518041a9acafb523455465dbdddc0ddad
Samuel Champagne <17770653+astraloverflow@users.noreply.github.com>
2025-10-30 11:36:39 -0500

    Update site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bookofsatoshi.com",
+  // site: "https://bookofsatoshi.com",
+  site: "https://e53publishing.github.io",
+  base: "/bookofsatoshi-v2",
   vite: {
     plugins: [tailwindcss()],
   },
